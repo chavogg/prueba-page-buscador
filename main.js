44d1150e0ab3b361b178bb80345897c78858ccd3
chavogg <68310028+chavogg@users.noreply.github.com>
2020-12-17 20:19:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var btnmenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+
+
+
+var btnmenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btnmenu.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');
+})
+
+
+
+/*
+var btnmenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btnmenu.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');
+})
+
+*/
